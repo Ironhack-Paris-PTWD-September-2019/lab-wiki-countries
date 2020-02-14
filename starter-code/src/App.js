ddee9ffb053ce2d647db02ecc4c543cc802a2230
dienamo <dienamoustapha@gmail.com>
2020-02-14 15:42:06 +0100

    debut routes

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import countriesList from './countries.json'
+import CountryDetail from './CountryDetail'
+import { Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
 
@@ -10,23 +13,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav class="navbar navbar-dark bg-primary mb-3">
-        <div class="container">
-          <a class="navbar-brand" href="/">WikiCountries</a>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          <a className="navbar-brand" href="/">WikiCountries</a>
         </div>
       </nav>
         {this.state.countries.map((country)=> {
           return(
-            <div class="col-5" style={{maxHeight: 90}}>
-            <div class="list-group">
-          <a class="list-group-item list-group-item-action" href="/ABW">{country.flag}  {country.name.common}</a>
+            <div className="col-5" style={{maxHeight: 90}}>
+            <div className="list-group">
+          <Link to='/CountryDetail' className="list-group-item list-group-item-action">{country.flag}  {country.name.common}</Link>
             </div>
           </div>
           )
         })}
+         <CountryDetail name = {this.state.countries[0].name.common} capital = {this.state.countries[0].capital}/>
+
+         <Switch>
+           <Route exact path='/CountryDetail' component={CountryDetail}/>
+         </Switch>
       </div>
     );
   }
 }
-
 export default App;
---
+++ starter-code/src/CountryDetail.js
@@ -0,0 +1,44 @@
+// components/Home.js
+
+import React from 'react';
+
+const CountryDetail = (props) => {
+  let {name , capital} = props
+  return (
+    <div class="col-7">
+    <h1>{name}</h1>
+    <table class="table">
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td style={{width: 30}}>Capital</td>
+          <td>{capital}</td>
+        </tr>
+        <tr>
+          <td>Area</td>
+          <td>551695 km
+            <sup>2</sup>
+          </td>
+        </tr>
+        <tr>
+          <td>Borders</td>
+          <td>
+            <ul>
+              <li><a href="/AND">Andorra</a></li>
+              <li><a href="/BEL">Belgium</a></li>
+              <li><a href="/DEU">Germany</a></li>
+              <li><a href="/ITA">Italy</a></li>
+              <li><a href="/LUX">Luxembourg</a></li>
+              <li><a href="/MCO">Monaco</a></li>
+              <li><a href="/ESP">Spain</a></li>
+              <li><a href="/CHE">Switzerland</a></li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  )
+}
+
+export default CountryDetail;
