3117f0dc6b287f60912b086466f8ef04be039866
marina <alvarez.marina7@hotmail.fr>
2020-02-15 11:54:57 +0100

    encore sur la 1ere iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import CountryList from "./components/CountryList";
+
+// import CountryDetails from './components/CountryDetails';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">WikiCountries</a>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={CountryList}/>
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
